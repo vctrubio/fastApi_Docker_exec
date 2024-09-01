9b943ad9162e591c3f60ce4c588369aaf1238a27
vctrubio <vctrubio@gmail.com>
2024-09-01 11:47:00 +0200

    redirecting stdout to null

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import os
+import io
+import sys
+
 import pandas
 import numpy
 import json
@@ -34,16 +37,23 @@ async def execute_script(request: Request):
 
     exec_var = {}
     try:
+        old_stdout = sys.stdout
+        sys.stdout = io.StringIO()
+        
         exec(script, {}, exec_var)  # exec(object[, globals[, locals]])
         if "main" in exec_var and callable(exec_var["main"]):
             result = exec_var["main"]()
         else:
             result = None
+            
+        sys.stdout = old_stdout
+
         response = {"result": result, "script": script}
         log_to_file(json.dumps(response))
         return response["result"]
 
     except Exception as e:
+        sys.stdout = old_stdout
         error_msg = {"error": str(e)}
         log_to_file(json.dumps(error_msg))
         return error_msg
