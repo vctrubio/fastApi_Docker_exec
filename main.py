8ec32650c7d209cf6cbb4abf4ece2345e57208ab
vctrubio <vctrubio@gmail.com>
2024-09-01 10:59:43 +0200

    testing if script is present in body

--- main.py
+++ main.py
@@ -9,10 +9,14 @@ import uvicorn
 app = FastAPI()
 
 @app.post("/execute")
-async def execute_script():
-    return {"message": "Script will executed"}
+async def execute_script(request: Request):
+    data = await request.json()
+    script = data.get("script")
 
+    if not script:
+        return {"error": "No script provided"}
 
+    return {"result": "script provided"}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8080)
